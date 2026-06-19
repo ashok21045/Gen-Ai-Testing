b2e2637d4d854a1a8c232fbc965f71b8b2a62fd4
Ashok Poudel <poudelashok222@gmail.com>
2026-06-19 15:40:42 +0545

    Added first file

--- first_ofsecond.py
+++ first_ofsecond.py
@@ -6,3 +6,9 @@ print("this is the first of the second person branch")
 # git pull origin develop --rebase
 # git rebase develop 
 # Successfully rebased and updated refs/heads/FE-Secondperson.
+
+
+# git checkout develop
+# git pull origin develop
+# git checkout second_preson
+# git push --force-with-lease origin second_preson
